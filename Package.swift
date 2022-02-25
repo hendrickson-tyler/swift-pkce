ee9b229a6300f911cb64c6cea6f222e79e778a3f
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2022-02-25 09:27:10 -0700

    chore: define supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "swift-pkce",
+    platforms: [
+        .iOS("13.0"),
+        .macOS("10.15"),
+        .macCatalyst("15.0"),
+        .tvOS("15.0"),
+        .watchOS("8.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
