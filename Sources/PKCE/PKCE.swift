b587b1925570f7165312b8bbe003e584fdca53db
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2022-02-25 14:53:22 -0700

    refactor: make functions available directly

--- Sources/PKCE/PKCE.swift
+++ Sources/PKCE/PKCE.swift
@@ -7,63 +7,6 @@ let bitsInOctet = 8
 let minCodeVerifierLength = 43
 let maxCodeVerifierLength = 128
 
-public struct PKCE {
-    
-    /// Generates a new, random code verifier.
-    /// - Parameter length: The number of characters for the code verifier. The code verifier must have a minimum of 43 characters and a maximum of 128 characters.
-    /// - Returns: The generated code verifier.
-    public static func generateCodeVerifier(length: Int = 128) throws -> String {
-        if length < minCodeVerifierLength || length > maxCodeVerifierLength {
-            throw PKCEError.invalidCodeVerifierLength
-        }
-        let octetCount = length * bitsInChar / bitsInOctet
-        let octets = try generateRandomOctets(octetCount: octetCount)
-        return encodeBase64URLString(octets: octets)
-    }
-    
-    
-    /// Generates a code challenge for a given code verifier.
-    /// - Parameter codeVerifier: The code verifier for which to generate a code challenge.
-    /// - Returns: The generated code challenge.
-    public static func generateCodeChallenge(for codeVerifier: String) throws -> String {
-        let challenge = codeVerifier
-            .data(using: .ascii)
-            .map { SHA256.hash(data: $0) }
-            .map { encodeBase64URLString(octets: $0) }
-        guard let challenge = challenge else {
-            throw PKCEError.failedToCreateCodeChallengeChallenge
-        }
-        return challenge
-    }
-    
-    
-    /// Generates a specified number of random octets.
-    /// - Parameter octetCount: The number of octets to generate.
-    /// - Returns: The randomly generated octets.
-    private static func generateRandomOctets(octetCount: Int) throws -> [UInt8] {
-        var octets = [UInt8](repeating: 0, count: octetCount)
-        let status = SecRandomCopyBytes(kSecRandomDefault, octets.count, &octets)
-        if status != errSecSuccess {
-            throw PKCEError.failedToGenerateRandomOctets
-        }
-        return octets
-    }
-    
-    
-    /// Encodes a sequence of octets as a Base64 URL string.
-    /// - Parameter octets: The octets to be encoded.
-    /// - Returns: The Base64 URL-encoded string.
-    private static func encodeBase64URLString<S>(octets: S) -> String where S: Sequence, UInt8 == S.Element {
-        let data = Data(octets)
-            return data
-                .base64EncodedString()
-                .replacingOccurrences(of: "=", with: "")
-                .replacingOccurrences(of: "+", with: "-")
-                .replacingOccurrences(of: "/", with: "_")
-                .trimmingCharacters(in: .whitespaces)
-    }
-}
-
 /// Errors that can occur when generating values for PKCE.
 public enum PKCEError: Error {
     /// Requested an invalid code verifier. The requested code verifier length is not within the range of 43 to 128.
@@ -72,4 +15,55 @@ public enum PKCEError: Error {
     case failedToGenerateRandomOctets
     /// An error occured when trying to genereate the code challenge for a given code verifier.
     case failedToCreateCodeChallengeChallenge
+}
+    
+/// Generates a new, random code verifier.
+/// - Parameter length: The number of characters for the code verifier. The code verifier must have a minimum of 43 characters and a maximum of 128 characters.
+/// - Returns: The generated code verifier.
+public func generateCodeVerifier(length: Int = 128) throws -> String {
+    if length < minCodeVerifierLength || length > maxCodeVerifierLength {
+        throw PKCEError.invalidCodeVerifierLength
+    }
+    let octetCount = length * bitsInChar / bitsInOctet
+    let octets = try generateRandomOctets(octetCount: octetCount)
+    return encodeBase64URLString(octets: octets)
+}
+
+/// Generates a code challenge for a given code verifier.
+/// - Parameter codeVerifier: The code verifier for which to generate a code challenge.
+/// - Returns: The generated code challenge.
+public func generateCodeChallenge(for codeVerifier: String) throws -> String {
+    let challenge = codeVerifier
+        .data(using: .ascii)
+        .map { SHA256.hash(data: $0) }
+        .map { encodeBase64URLString(octets: $0) }
+    guard let challenge = challenge else {
+        throw PKCEError.failedToCreateCodeChallengeChallenge
+    }
+    return challenge
+}
+
+/// Generates a specified number of random octets.
+/// - Parameter octetCount: The number of octets to generate.
+/// - Returns: The randomly generated octets.
+private func generateRandomOctets(octetCount: Int) throws -> [UInt8] {
+    var octets = [UInt8](repeating: 0, count: octetCount)
+    let status = SecRandomCopyBytes(kSecRandomDefault, octets.count, &octets)
+    if status != errSecSuccess {
+        throw PKCEError.failedToGenerateRandomOctets
+    }
+    return octets
+}
+
+/// Encodes a sequence of octets as a Base64 URL string.
+/// - Parameter octets: The octets to be encoded.
+/// - Returns: The Base64 URL-encoded string.
+private func encodeBase64URLString<S>(octets: S) -> String where S: Sequence, UInt8 == S.Element {
+    let data = Data(octets)
+        return data
+            .base64EncodedString()
+            .replacingOccurrences(of: "=", with: "")
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .trimmingCharacters(in: .whitespaces)
 }
--- Tests/PKCETests/PKCETests.swift
+++ Tests/PKCETests/PKCETests.swift
@@ -4,24 +4,24 @@ import XCTest
 final class PKCETests: XCTestCase {
     
     func testInvalidLengthError() throws {
-        XCTAssertThrowsError(try PKCE.generateCodeVerifier(length: 42), "Generating a code verifier with a length of 43 didn't throw an error") { error in
+        XCTAssertThrowsError(try generateCodeVerifier(length: 42), "Generating a code verifier with a length of 43 didn't throw an error") { error in
             XCTAssertEqual(error as! PKCEError, PKCEError.invalidCodeVerifierLength, "The thrown error wasn't for invalid length")
         }
-        XCTAssertThrowsError(try PKCE.generateCodeVerifier(length: 129), "Generating a code verifier with a length of 129 didn't throw an error") { error in
+        XCTAssertThrowsError(try generateCodeVerifier(length: 129), "Generating a code verifier with a length of 129 didn't throw an error") { error in
             XCTAssertEqual(error as! PKCEError, PKCEError.invalidCodeVerifierLength, "The thrown error wasn't for invalid length")
         }
     }
     
     func testCodeVerifierLength() throws {
-        var codeVerifier = try PKCE.generateCodeVerifier(length: 43)
+        var codeVerifier = try generateCodeVerifier(length: 43)
         XCTAssertEqual(codeVerifier.count, 43, "Code verifier length was not 43")
-        codeVerifier = try PKCE.generateCodeVerifier(length: 128)
+        codeVerifier = try generateCodeVerifier(length: 128)
         XCTAssertEqual(codeVerifier.count, 128, "Code verifier length was not 128")
     }
     
     func testCodeChallengeGeneration() throws {
         let codeVerifier = "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
-        XCTAssertEqual(try PKCE.generateCodeChallenge(for: codeVerifier), "E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM", "Code challenge wasn't generated correctly")
+        XCTAssertEqual(try generateCodeChallenge(for: codeVerifier), "E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM", "Code challenge wasn't generated correctly")
     }
 
 }
