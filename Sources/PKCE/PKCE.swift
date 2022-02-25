7fbf6a1ab97f96d698bd9dac6a2c2421c27dfc70
Tyler Hendrickson <6372535+hendrickson-tyler@users.noreply.github.com>
2022-02-24 19:33:46 -0700

    chore: rename targets

--- Sources/PKCE/PKCE.swift
+++ Sources/PKCE/PKCE.swift
@@ -1,4 +1,4 @@
-public struct swift_pkce {
+public struct PKCE {
     public private(set) var text = "Hello, World!"
 
     public init() {
--- Tests/PKCETests/PKCETests.swift
+++ Tests/PKCETests/PKCETests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import swift_pkce
+@testable import PKCE
 
-final class swift_pkceTests: XCTestCase {
+final class PKCETests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_pkce().text, "Hello, World!")
+        XCTAssertEqual(PKCE().text, "Hello, World!")
     }
 }
